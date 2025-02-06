7b9f1c90dce129a17420c6334cf78596b1e55c6e
mdriyajulislamakash11 <hafejmohammadakash@gmail.com>
2025-02-07 02:02:39 +0600

    donnnnne

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// array-looping-tasks
-// Task 1
-// Write a JavaScript code to reverse the array colors 
-// without using the reverse method.
+array-looping-tasks
+Task 1
+Write a JavaScript code to reverse the array colors 
+without using the reverse method.
 
-// Input: 
+Input: 
 const colors = ['red', 'blue', 'green', 'yellow', 'orange']
 
 const output = [];
@@ -13,11 +13,32 @@ for (let i = 1; i < colors.length; i++) {
 console.log(output)
 
 const ot = []
-for(let l = colors.length - 1; i <= 0; l--){
-      ot.unshift(colors[l0 ])
+for (let l = colors.length - 1; i <= 0; l--) {
+      ot.unshift(colors[l0])
 }
 
 
-// // Output:
+// Output:
 
-// ['orange', 'yellow', 'green', 'blue', 'red']
+['orange', 'yellow', 'green', 'blue', 'red']
+
+
+function savings(income) {
+      if (typeof income !== "number") {
+            return "ivalid iput"
+      }
+
+      const ghorBhara = 5000;
+      const khawa = 3000;
+      const hatKhoroc = 2000;
+      const totalKhoroc = ghorBhara + khawa + hatKhoroc;
+
+      if(totalKhoroc > income){
+            return "Earn more..."
+      }
+
+      const result = income - totalKhoroc;
+      return result
+}
+const result = savings(20000);
+console.log(result)
